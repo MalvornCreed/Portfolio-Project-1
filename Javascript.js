57301f273862e00da0ae66d60096d87ef7705bdc
Malvorn Creed <149461437+MalvornCreed@users.noreply.github.com>
2024-05-04 07:55:35 +0200

    second commit

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+function timer(){
+    var sec = 30;
+    var timer = setInterval(function(){
+        document.getElementById('timer').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+window.onload(timer())
